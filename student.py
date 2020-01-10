3597dd4aa79bfc168414909668effe7bc384253a
KwonKusang <rnjsrntkd95@gmail.com>
2020-01-10 18:33:41 +0900

    added login file and check main file

--- student.py
+++ student.py
@@ -1,6 +1,7 @@
 class student:
     def __init__(self, st_name):
         self.st_name = st_name
-        self.st_coding = 100
-        self.st_fatigue = 100
+        self.st_coding = 0
+        self.st_fatigue = 0
         self.st_humanity = 100
+        self.st_passion = 100
